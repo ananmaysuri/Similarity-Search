39bffca31c33f15347ea9340622af84c649ee571
Ananmay Suri <ananmaysuri2000@gmail.com>
2022-04-12 17:44:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.header("Similarity Search - Major Project Testing")
 imageText = st.text_input("Input Query Text")
 N = st.text_input("Input Page Number")
 if imageText:
-    with st.spinner(text = 'Getting Images from Unsplash and Sorting with CLIP ...'):
+    with st.spinner(text = 'Getting Images from Google and Sorting with CLIP ...'):
         
         imgSimScore, upSplashImages = getSortedQuery(imageText, N)
         images = [linkToImage(img) for img, score in imgSimScore]
@@ -20,5 +20,5 @@ if imageText:
         col1, col2 = st.beta_columns(2)
         col1.header("Similarity Search")
         col1.image(images, width = 300, caption = simScore)
-        col2.header("Images from Unsplash")
+        col2.header("Images from Google")
         col2.image(upSplashImages, width = 300, caption = upSplashIx)
